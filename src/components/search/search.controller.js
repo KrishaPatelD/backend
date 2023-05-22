c346a23896ac2b0fe0a6d6a32743072578f6a199
krisha.patel <krisha.patel@volansys.com>
2023-05-22 18:45:49 +0530

    added backend

---
+++ src/components/search/search.controller.js
@@ -0,0 +1,38 @@
+const Question = require("../../models/question");
+
+// search question
+exports.searchQuestion = async (req, res) => {
+  try {
+    const {question} = req.query;
+    const searchedData = await Question.aggregate([
+        {
+          $search: {
+            index: "search-question",
+            text: {
+              query: question,
+              path:  "question"
+            }
+          }
+        }
+      ])
+
+    if (!searchedData) {
+      return res.status(404).json({
+        status: 404,
+        message: "Data Not Found",
+      });
+    } else {
+      res.status(200).json({
+        status: 200,
+        message: "Qustion searched Successfully",
+        data: searchedData,
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({
+      status: 500,
+      message: "Server Error",
+    });
+  }
+};
